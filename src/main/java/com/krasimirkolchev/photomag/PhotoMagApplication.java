daa8ce1ae068a8addbc23af3062fc93ec4390b54
KrasimirKolchev <krasimir.kolchev2087@gmail.com>
2020-07-10 18:13:23 +0300

    Initial commit

---
+++ src/main/java/com/krasimirkolchev/photomag/PhotoMagApplication.java
@@ -0,0 +1,13 @@
+package com.krasimirkolchev.photomag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoMagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoMagApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/krasimirkolchev/photomag/PhotoMagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krasimirkolchev.photomag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoMagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
